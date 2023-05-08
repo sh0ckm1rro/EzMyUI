58fe03c3c2fd42e6951f692442d48773911f2639
ShockMirro <79736345+sh0ckm1rro@users.noreply.github.com>
2023-05-08 21:31:55 +0800

    Update FONT.lua
    10.0 更新 SetFont("font",size[,"flags"])

--- FONT.lua
+++ FONT.lua
@@ -1,10 +1,12 @@
-﻿local cfg = {
+--TrackerFrame 任務
+local cfg = {
 font = STANDARD_TEXT_FONT,
 }
-QuestTitleFont:SetFont(cfg.font,18)		--任務名稱
-QuestFont:SetFont(cfg.font, 18)		--任務敘述
-QuestFontNormalSmall:SetFont(cfg.font, 16)	--完成目標
-MailTextFontNormal:SetFont(cfg.font, 18)	--信件內文
+--local function defaultsetting()
+QuestTitleFont:SetFont(cfg.font, 20,"")		--任務名稱
+QuestFont:SetFont(cfg.font, 18,"")		--任務敘述
+QuestFontNormalSmall:SetFont(cfg.font, 18,"")	--完成目標
+--end
 
 --字體大小描邊
 local function SetFont(obj, optSize)
@@ -12,8 +14,10 @@ local fontName, _,fontFlags  = obj:GetFont()
 	obj:SetFont(fontName,optSize,"OUTLINE")
 	obj:SetShadowOffset(0, 0)
 end
---SetFont(GameFontNormalSmall, 16)		--ID&快捷列&聊天分頁&聊天&公會新聞&預組
+SetFont(GameFontNormalSmall, 16)		--ID&快捷列&聊天分頁&公會新聞&預組
 SetFont(GameFontNormal, 17)			--任務欄&技能
-SetFont(SystemFont_LargeNamePlateFixed,14)
-SetFont(SystemFont_NamePlateFixed,14)
-SetFont(ErrorFont, 18)
+SetFont(SystemFont_LargeNamePlateFixed,14)--大型名條名稱
+SetFont(SystemFont_LargeNamePlate,14)
+SetFont(SystemFont_NamePlate,10)
+SetFont(SystemFont_NamePlateFixed,10)	--小型名條名稱
+SetFont(ErrorFont, 18)			--錯誤字體
